b02e01215d8e90b74ad3e0362842e506cdeab91f
devhimelali <dev.himelali@gmail.com>
2025-08-25 10:59:29 +0600

    feat: add archiving logic for health and safety cross criteria, including related model updates and helper methods

--- app/Services/ArchieService.php
+++ app/Services/ArchieService.php
@@ -2,6 +2,9 @@
 
 namespace App\Services;
 
+use App\Models\CrossCriteria;
+use App\Models\HealthSafetyCrossCriteria;
+use App\Models\HealthSafetyCrossCriteriaArchive;
 use App\Models\HealthSafetyReview;
 use App\Models\HealthSafetyReviewArchive;
 use App\Models\LabourShift;
@@ -42,6 +45,36 @@ class ArchieService
         }
     }
 
+    public function archivedHealthAndSafetyCrossCriteria($dates)
+    {
+        foreach ($dates as $date) {
+            $crossCriteria = HealthSafetyCrossCriteria::where('date', $date)->get();
+            foreach ($crossCriteria as $criteria) {
+                $crew = $this->getShiftName($criteria->shift_id);
+                $shiftRotation = $this->getShiftRotationDay($criteria->shift_rotation_id);
+                $criteriaDetail = $this->getCrossCriteria($criteria->cross_criteria_id);
+                $supervisor = $this->getSupervisorName($date, $criteria->shift_type);
+                $labour = $this->getLabourNames($date, $criteria->shift_type);
+
+                HealthSafetyCrossCriteriaArchive::create([
+                    'criteria_name' => $criteriaDetail->name,
+                    'criteria_description' => $criteriaDetail->description,
+                    'criteria_color' => $criteriaDetail->color,
+                    'criteria_bg_color' => $criteriaDetail->bg_color,
+                    'crew' => $crew,
+                    'shift_rotation' => $shiftRotation,
+                    'start_date' => $criteria->start_date,
+                    'end_date' => $criteria->end_date,
+                    'shift_type' => $criteria->shift_type,
+                    'date' => $criteria->date,
+                    'cell_number' => $criteria->cell_number,
+                    'supervisor_name' => $supervisor,
+                    'labour_name' => $labour,
+                ]);
+            }
+        }
+    }
+
     /**
      * Get the shift name by id
      * @param $id
@@ -62,6 +95,15 @@ class ArchieService
         return ShiftRotation::find($id)?->day;
     }
 
+    /**
+     * Get the cross criteria by id
+     * @param $id
+     * @return mixed
+     */
+    private function getCrossCriteria($id)
+    {
+        return CrossCriteria::find($id);
+    }
 
     /**
      * Get the supervisor name by date and shift
