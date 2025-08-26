dec96c2a4d4d2d411336e704b3363ae753d31b2a
devhimelali <dev.himelali@gmail.com>
2025-08-26 16:57:30 +0600

    feat: extend archival logic in ArchiveController
    - Replaced hardcoded date with dynamic shift date retrieval.
    - Added archival logic for reviewing the previous shift.

--- app/Http/Controllers/admin/ArchiveController.php
+++ app/Http/Controllers/admin/ArchiveController.php
@@ -17,12 +17,13 @@ class ArchiveController extends Controller
 
     public function archivedAllBoards(Request $request)
     {
-//        $date = $this->getShiftDate();
-        $date = Carbon::create(2025, 8, 26)->format('Y-m-d');
-
+        $date = $this->getShiftDate();
+//        $date = Carbon::create(2025, 8, 26)->format('Y-m-d');
         $shift_type = $request->header('shift-type') ?? 'day';
+
         $this->archieService->archivedHealthAndSafetyReview($date, $shift_type);
         $this->archieService->archivedHealthAndSafetyCrossCriteria($date, $shift_type);
+        $this->archieService->archivedReviewPreviousShift($date, $shift_type);
         $this->archieService->archivedCelebrateSuccess($date, $shift_type);
         $this->archieService->archivedSiteCommunication($date, $shift_type);
         $this->archieService->archivedShiftLog($date, $shift_type);
